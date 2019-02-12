12fc7c4b7fdf86747b4a6d6dcf8f0e811e38019f
V-Jayesh-R <jvkillercool@gmail.com>
2019-02-12 11:52:12 +0530

    task2

---
+++ src/app/Item.ts
@@ -0,0 +1,11 @@
+export class Item{
+    checked: boolean;
+    desc: string;
+    code: string;
+    details: string;
+    uom: string;
+    quantity: string;
+    delivery: string;
+    template: string;
+    delete: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { TableComponent } from './table/table.component';
+import { DatecompComponent } from './datecomp/datecomp.component';
+import { FormComponent } from './form/form.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    TableComponent,
+    DatecompComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule
   ],
-  
-  providers: [],
+  providers: [MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
